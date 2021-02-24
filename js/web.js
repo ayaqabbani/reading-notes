cfe5fa074a62a2fa209ce0138109fd27ff697a6a
ayaqabbani <ayaqabbani998@gmail.com>
2021-02-24 19:52:39 +0200

    dadded functions

--- js/web.js
+++ js/web.js
@@ -1,26 +1,41 @@
 // giving a survey for my educational website students 
-// give the access to the survey only for programing student 2020
+// give the access to the survey only for students who got more of 50% of the full mark
 
-var studentMajor="programing"
-var studentYear="2020"
+// document.getElementById("allow").innerHTML;
+function avgCalculator(Mark1, Mark2, Mark3) {
+    var sum = parseInt(Mark1) + parseInt(Mark2) + parseInt(Mark3);
+    var avg = sum / 3;
+    alert("your total mark is" + avg);
+    if (avg >= 50) {
+        alert("please fill the survey");
+    } else if (avg < 50) {
+
+        alert("this survey is for students who passed only;")
+        document.getElementById('page').innerText = ''
+    }
+
+}
+var counter = 0;
+var Mark1 = 0;
+var Mark2 = 0;
+var Mark3 = 0;
 
 confirm("are you one of our students?");
+var studentName = prompt("what's your name?");
 
-var studentName=prompt("what's your name?");
-console.log("welcome");
+while (counter < 3) {
+    if (counter === 0) {
+        Mark1 = prompt("what was your mark in first exam?");
+    } else if (counter == 1) {
+        Mark2 = prompt("what was your mark in med exam?");
+    } else {
+        Mark3 = prompt("what was your mark in final exam?");
+    }
 
-var studentMajorInput=prompt("what's your major?").toLowerCase();
+    counter += 1;
 
-if (studentMajorInput===studentMajor){
-    console.log("you're free to continue");
-}else {
-    alert("this survey is only for programing students");
 }
+avgCalculator(Mark1, Mark2, Mark3);
 
-var year=prompt("entry year?");
-if (year===studentYear){
-    alert("please fill the survey");
-}else {
-    alert("this survey is for 2020 students only;")
-}
+
 
